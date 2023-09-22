f514106173ea3260dc141e3ef4ee7d0d4ce865a0
rohitc33 <70339497+rohitc33@users.noreply.github.com>
2023-09-21 23:07:48 -0500

    Add database

--- db.py
+++ db.py
@@ -1,19 +1,34 @@
 from flask_login import UserMixin
+from google.cloud import ndb
 
 
-users = {}
+client = ndb.Client()
 
 
-class User(UserMixin):
-    def __init__(self, id_, name, email):
-        self.id = id_
-        self.name = name
-        self.email = email
-        users[id_] = self
+class User(ndb.Model):
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
 
-    @staticmethod
-    def get(id_=None):
-        print(id_)
-        if id_ is None:
-            return None
-        return users[id_]
+
+class LoggedInUser(UserMixin):
+    def __init__(self, db_user):
+        self.id = db_user.key.id()
+        self.name = db_user.name
+        self.email = db_user.email
+
+
+def add_user(id, name, email):
+    with client.context():
+        user = User(id=id, name=name, email=email)
+        user.put()
+    return LoggedInUser(user)
+
+
+def get_user(id=None):
+    if id is None:
+        return None
+    with client.context():
+        user = User.get_by_id(id)
+    if user is None:
+        return None
+    return LoggedInUser(user)
