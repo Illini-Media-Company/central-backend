b202d7cc39b050fbb69d526ee9a933ee45de91f9
Prashant Naganaboyina <pkn3@illinois.edu>
2023-10-17 13:04:33 -0500

    skeleton, no styling

--- db/illordle_word.py
+++ db/illordle_word.py
@@ -1,27 +1,34 @@
-from flask_login import UserMixin
 from google.cloud import ndb
 
 from . import client
 
 
 class IllordleWord(ndb.Model):
-    date = ndb.DateProperty()
+    date = ndb.StringProperty()
     word = ndb.StringProperty()
 
 
 def add_word(word, date):
     with client.context():
-        illordle_word = IllordleWord.query().filter(IllordleWord.date == date)
+        query = IllordleWord.query().filter(IllordleWord.date == date)
+        illordle_word = query.get()
         if illordle_word is not None:
             illordle_word.key.delete()
         illordle_word = IllordleWord(date=date, word=word)
         illordle_word.put()
-
-    return illordle_word
+        return illordle_word.to_dict()
 
 
 def get_word(date):
     with client.context():
-        illordle_word = IllordleWord.query().filter(IllordleWord.date == date)
+        query = IllordleWord.query(IllordleWord.date == date)
+        word = query.get()
+        if word is None:
+            return None
+        return word.to_dict()
 
-    return illordle_word
+
+def get_all_words():
+    with client.context():
+        words = [word.to_dict() for word in IllordleWord.query().fetch()]
+    return words
--- main.py
+++ main.py
@@ -4,19 +4,13 @@ import os
 import urllib
 
 from dotenv import load_dotenv
-from flask import (
-    Flask,
-    redirect,
-    render_template,
-    request,
-    url_for
-)
+from flask import Flask, redirect, render_template, request, url_for
 from flask_login import (
     LoginManager,
     current_user,
     login_required,
     login_user,
-    logout_user
+    logout_user,
 )
 from flask_seasurf import SeaSurf
 from oauthlib.oauth2 import WebApplicationClient
@@ -32,12 +26,12 @@ from views.users import users_routes
 
 
 load_dotenv()
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
-GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
 
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 app.register_blueprint(illordle_routes)
 app.register_blueprint(socials_routes)
 app.register_blueprint(users_routes)
@@ -61,50 +55,50 @@ def load_user(user_id):
 
 @login_manager.unauthorized_handler
 def unauthorized_callback():
-    return redirect('/login?state=' + urllib.parse.quote(request.path))
+    return redirect("/login?state=" + urllib.parse.quote(request.path))
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    state = request.args.get('state')
+    state = request.args.get("state")
 
     # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg['authorization_endpoint']
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
     # Use library to construct the request for Google login and provide
     # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=request.base_url + '/callback',
-        scope=['openid', 'email', 'profile'],
-        state=state
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+        state=state,
     )
     return redirect(request_uri)
 
 
-@app.route('/login/callback')
+@app.route("/login/callback")
 def callback():
     # Get authorization code Google sent back to you
-    code = request.args.get('code')
-    state = request.args.get('state')
+    code = request.args.get("code")
+    state = request.args.get("state")
 
     # Find out what URL to hit to get tokens that allow you to ask for
     # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg['token_endpoint']
+    token_endpoint = google_provider_cfg["token_endpoint"]
 
     # Prepare and send a request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
         redirect_url=request.base_url,
-        code=code
+        code=code,
     )
     token_response = requests.post(
         token_url,
@@ -119,25 +113,28 @@ def callback():
     # Now that you have tokens (yay) let's find and hit the URL
     # from Google that gives you the user's profile information,
     # including their Google profile image and email
-    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body).json()
 
     # You want to make sure their email is verified.
     # The user authenticated with Google, authorized your
     # app, and now you've verified their email through Google!
-    if userinfo_response.get('email_verified'):
-        unique_id = userinfo_response['sub']
-        users_email = userinfo_response['email']
-        users_name = userinfo_response['name']
+    if userinfo_response.get("email_verified"):
+        unique_id = userinfo_response["sub"]
+        users_email = userinfo_response["email"]
+        users_name = userinfo_response["name"]
 
         # Create a user in your db with the information provided by Google
         user = get_user(users_email)
         if user is None:
-            if users_email.endswith('@illinimedia.com'):
+            if users_email.endswith("@illinimedia.com"):
                 user = add_user(sub=unique_id, name=users_name, email=users_email)
             else:
-                return 'User email must end with @illinimedia.com for automatic registration.', 403
+                return (
+                    "User email must end with @illinimedia.com for automatic registration.",
+                    403,
+                )
         elif user.sub is None:
             user = add_user(sub=unique_id, name=users_name, email=users_email)
 
@@ -150,37 +147,39 @@ def callback():
             if not (parsed_url.scheme or parsed_url.netloc):
                 return redirect(url)
             else:
-                return 'Illegal redirect URL.', 400
+                return "Illegal redirect URL.", 400
 
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        return 'User email not available or not verified by Google.', 400
+        return "User email not available or not verified by Google.", 400
 
 
-@app.route('/api-query')
+@app.route("/api-query")
 @login_required
 def api_query():
-    return render_template('api_query.html')
+    return render_template("api_query.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
-    if current_user.email.endswith('@illinimedia.com'):
+    if current_user.email.endswith("@illinimedia.com"):
         logout_user()
-        return redirect(url_for('yurr'))
+        return redirect(url_for("yurr"))
     else:
         logout_user()
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
 
-@app.route('/logout-success')
+@app.route("/logout-success")
 def yurr():
-    return render_template('yurr.html')
+    return render_template("yurr.html")
 
 
-if __name__ == '__main__':
-    if os.environ.get('DATASTORE_EMULATOR_HOST') is None:
-        logging.fatal('DATASTORE_EMULATOR_HOST environment variable must be set!')
+if __name__ == "__main__":
+    if os.environ.get("DATASTORE_EMULATOR_HOST") is None:
+        logging.fatal("DATASTORE_EMULATOR_HOST environment variable must be set!")
         exit(1)
-    app.run(port=5001, ssl_context='adhoc')
+    app.jinja_env.auto_reload = True
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run(port=5001, ssl_context="adhoc")
