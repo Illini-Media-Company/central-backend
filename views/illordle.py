617d0e7276cc30e50fffc75abc3e072619a04ff1
Jenny Mai <nhumai2@illinois.edu>
2023-11-06 16:06:33 -0600

    illordle api & dashboard

--- views/illordle.py
+++ views/illordle.py
@@ -1,51 +1,70 @@
 import sys
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, abort
 from flask_login import login_required
-
-from db.illordle_word import add_word, get_word, get_all_words
-
+from datetime import datetime, timedelta
+from db.illordle_word import add_word, get_word, get_all_words, delete_all_words
 
 illordle_routes = Blueprint("illordle_routes", __name__, url_prefix="/illordle")
-
 
 @illordle_routes.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template("illordle.html")
+    today = datetime.now().date()
+    next_seven_days = [today + timedelta(days=i) for i in range(8)]
+    words = []
+    for date in next_seven_days:
+        word = get_word(date)
+        if word != "No word available for that date.":
+            words.append(word)
+        else:
+            words.append({"date": date, "word": ""})
 
+    return render_template("illordle.html", words=words)
 
 @illordle_routes.route("/word", methods=["POST"])
 @login_required
 def create_word():
     word = request.form["word"]
-    date = request.form["date"]
-
-    valid_word, message = validate_word(word)
-    if not valid_word:
-        return message
+    date_str = request.form["date"]
+    try:
+        date = datetime.strptime(date_str, "%m/%d/%Y").date()
+    except ValueError: # HTML depends on these error messages. Check before modify.
+        return "ERROR: Invalid date format. Please use MM/DD/YYYY format."
+    if date < datetime.now().date():
+        return "ERROR: Date cannot be in the past."
+    if len(word) < 3 or len(word) > 7:
+        return "ERROR: Word must be longer than 2 characters and shorter than 7 characters."
 
     ret = add_word(word=word, date=date)
-    return f"Created word: {ret}", 200
-
+    return ret, 200
 
 @illordle_routes.route("/word", methods=["GET"])
 @login_required
+def list_word():
+    date_str = request.args.get("date")
+    print(date_str)
+    if date_str:
+        try:
+            date = datetime.strptime(date_str, "%m/%d/%Y").date()
+            word = get_word(date)
+            return word
+        except ValueError:
+            return "Invalid date format. Please use MM/DD/YYYY format.", 400
+    else:
+        return "No date was provided to search.", 200
+
+@illordle_routes.route("/all_words", methods=["GET"])
+@login_required
 def list_words():
-    if "date" in request.args:
-        date = request.args.get("date")
-        return get_word(date=date)
-
-    return get_all_words()
-
-
-"""
-Helper/Utils
-"""
+    words = get_all_words()
+    if words:
+        return words
+    else:
+        return "No words in database.", 404
 
 
-def validate_word(word):
-    if len(word) < 2 or len(word) > 7:
-        return (
-            False,
-            "word must be greater than 2 characters and less than 7 characters.",
-        )
+@illordle_routes.route('/delete_all_words', methods=['POST'])
+@login_required
+def delete_words():
+    delete_all_words()
+    return 'All words deleted.'
