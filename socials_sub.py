4a3f2d33fb53292b8170654a4ec60ee169b8fd3a
ncald23 <norma.cval.23@gmail.com>
2023-11-29 19:58:28 -0600

    commit

--- socials_sub.py
+++ socials_sub.py
@@ -1,51 +1,53 @@
-from flask import Flask, request, render_template
-import praw
-
-app = Flask(__name__)
-
-# Reddit app credentials
-client_id = 'tJADWyLTm9saDIIYbPq8ow'
-client_secret = 'lD7Qdk8NB5hMflJe7R1lH0MQXpm4oQ'
-user_agent = 'story submission by u/Frogznlogs12'
-username = 'Frogznlogs12'
-password = 'NTS@5itwas'
-
-# Reddit API wrapper
-reddit = praw.Reddit(
-    client_id=client_id,
-    client_secret=client_secret,
-    user_agent=user_agent,
-    username=username,
-    password=password
-)
-
-# test subreddit
-subreddit_name = 'r/test_com23'
-
-# URL input
-url = input("Enter the URL: ")
-
-# Submit URL as link post to Reddit
-reddit.subreddit(subreddit_name).submit(url, url=url)
-
-print(f"Posted to r/{subreddit_name}: {url}")
 
 
+from flask import Flask, render_template, request, redirect, url_for
+import pra
+f
 
-#using flask, question is how
+app = Flask("submit_reddit")
+
+def post_to_reddit(title, url, subreddit, client_id, client_secret, user_agent, reddit_username, reddit_password):
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent,
+        username=reddit_username,
+        password=reddit_password
+    )
+
+    try:
+        subreddit = reddit.subreddit(subreddit)  # Fix: Use the provided subreddit parameter
+        submission = subreddit.submit(title, url=url)
+        return submission.url
+    except Exception as e:
+        return str(e)
+
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def submit_story():
     if request.method == 'POST':
-        # Retrieve data from the form
+        title = request.form['title']
         url = request.form['url']
+        subreddit = request.form['test_com23']
 
-        # Submit the URL as a link post to Reddit
-        subreddit = reddit.subreddit('YOUR_SUBREDDIT')
-        subreddit.submit(url, url=url)
+        # Reddit API credentials
+        client_id = 'tJADWyLTm9saDIIYbPq8ow'
+        client_secret = 'lD7Qdk8NB5hMflJe7R1lH0MQXpm4oQ'
+        user_agent = 'story submission by u/Frogznlogs12'
+        reddit_username = 'Frogznlogs12'
+        reddit_password = 'NTS@5itwas'
 
-        return 'URL submitted to Reddit successfully!'
+        submission_url = post_to_reddit(title, url, subreddit, client_id, client_secret, user_agent, reddit_username, reddit_password)
+        print(submission_url)
+        if submission_url:
+            return redirect(url_for('socials_routes.dashboard', url=submission_url))
+        else:
+            return "Error posting to Reddit."
 
-    return render_template('index.html')
+    return render_template('socials.html')
+
+@app.route('/success/<url>')
+def success(url):
+    return render_template('confirmation_socials_.html')  # Fix: Correct the template name
 
 if __name__ == '__main__':
     app.run(debug=True)
--- views/socials.py
+++ views/socials.py
@@ -1,14 +1,58 @@
 from flask import (
     Blueprint,
-    render_template
+    render_template,
+    request,
+    redirect,
+    url_for
 )
+
 from flask_login import login_required
+import praw
 
 
 socials_routes = Blueprint('socials_routes', __name__, url_prefix='/socials')
 
+def post_to_reddit(title, url, subreddit, client_id, client_secret, user_agent, reddit_username, reddit_password):
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent,
+        username=reddit_username,
+        password=reddit_password
+    )
+    try:
+        subreddit = reddit.subreddit(subreddit)  # Fix: Use the provided subreddit parameter
+        submission = subreddit.submit(title, url=url)
+        return submission.url
+    except Exception as e:
+        return str(e)
 
 @socials_routes.route('/dashboard')
 @login_required
 def dashboard():
+    return render_template('socials.html')
+
+@socials_routes.route('/submit-story', methods=['POST'])
+def submit_story():
+    print('text')
+    if request.method == 'POST':
+        title = request.form['title']
+        url = request.form['url']
+        subreddit = 'test_com23'
+
+        # Reddit API credentials
+        client_id = 'cWPduh2pXLRa_4Ku8lxY6A'
+        client_secret = '7F9Xw6cbZ3pqxaoZFNdZ4eCuvxZCKg'
+        user_agent = 'story submission by u/Frogznlogs12'
+        reddit_username = 'Frogznlogs12'
+        reddit_password = 'NTS@5itwas'
+
+        submission_url = post_to_reddit(title, url, subreddit, client_id, client_secret, user_agent, reddit_username, reddit_password)
+        print('anything')
+        print(submission_url)
+        if submission_url:
+            return redirect(url_for('socials_routes.dashboard', url=submission_url))
+        else:
+            return "Error posting to Reddit."
+
     return render_template('socials.html')
