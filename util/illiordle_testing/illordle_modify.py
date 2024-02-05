71f66a40abf0fa1e0391049f6ebc3091bea38a30
ncald23 <norma.cval.23@gmail.com>
2024-02-05 13:14:59 -0600

    Illordle generating random word for today

---
+++ util/illiordle_testing/illordle_modify.py
@@ -0,0 +1,14 @@
+from rand_wordle_list import word_random
+
+
+
+
+def random_words():
+        word = word_random()
+        return word
+
+
+print(random_words())
+# random word from word list (decide how to implement this)
+
+
