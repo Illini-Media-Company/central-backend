77ed27156468bb1e7dd919529060a387a258c771
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2024-02-05 17:16:55 -0600

    Moved backend logic to stories.py

--- util/stories.py
+++ util/stories.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import praw
 import requests
+import re
+from flask import jsonify
 
 from constants import (
     REDDIT_USERNAME,
@@ -11,7 +13,6 @@ from constants import (
     FLAIR_ID,
 )
 
-
 def get_title_from_url(url):
     try:
         response = requests.get(url + "feed/?withoutcomments=1")
@@ -19,7 +20,6 @@ def get_title_from_url(url):
         return soup.find("channel").find("item").find("title").text
     except:
         return None
-
 
 def post_to_reddit(title, url):
     reddit = praw.Reddit(
@@ -32,3 +32,24 @@ def post_to_reddit(title, url):
     subreddit = reddit.subreddit(SUBREDDIT)
     submission = subreddit.submit(title, url=url, flair_id=FLAIR_ID)
     return "https://www.reddit.com" + submission.permalink
+
+
+def get_published_url(url):
+    post_id_match = re.search(r'post=(\d+)', url)
+    if not post_id_match:
+        return jsonify({"error": "Invalid URL format. Please enter a valid URL."}), 400
+
+    post_id = post_id_match.group(1)
+
+    api_url = f"https://dailyillini.com/wp-json/wp/v2/posts/{post_id}"
+    response = requests.get(api_url)
+
+    data = response.json()
+
+    if "code" in data and data["code"] == "rest_post_invalid_id":
+        return jsonify({"result": "No publication could be found"})
+
+    if "link" in data:
+        return jsonify({"result": data["link"]})
+    else:
+        return jsonify({"result": "Unexpected response from the server"}), 500
