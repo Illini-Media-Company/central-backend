4bdd173c7578c5b349fc1b941766dae7b2e06187
TalhaAli <tlha2422@gmail.com>
2024-02-07 16:47:33 -0600

    added constant contact interface on dashboard and created functions that create and store auth code

--- constants.py
+++ constants.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ENV = os.environ.get("ENV", "dev")
+ENV = "dev" #os.environ.get("ENV", "dev")
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_CLIENT_ID = "***REMOVED***"#os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = "***REMOVED***"#os.environ.get("GOOGLE_CLIENT_SECRET", None)
 
 REDDIT_USERNAME = "TheDailyIllini"
 REDDIT_PASSWORD = os.environ.get("REDDIT_PASSWORD", None)
@@ -19,3 +19,6 @@ FLAIR_ID = "a3994b2e-d384-11ea-bf32-0e7e74729027"  # News flair
 
 SNO_ADMIN_USERNAME = "di_admin"
 SNO_ADMIN_PASSWORD = os.environ.get("SNO_ADMIN_PASSWORD", None)
+
+CC_CLIENT_ID = os.environ.get("CC_CLIENT_ID", None)
+CC_CLIENT_SECRET = os.environ.get("CC_CLIENT_SECRET", None)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 import urllib
+import base64
+
 
 from flask import (
     Flask,
@@ -32,17 +34,26 @@ from db.user import (
     get_user,
     update_user_groups,
 )
-from util.security import get_google_provider_cfg, get_groups_for_user, require_internal
+from util import (
+    get_google_provider_cfg,
+    get_groups_for_user,
+)
+from db.kv_store import (
+    kv_store_get,
+    kv_store_set
+)
 from views.illordle import illordle_routes
 from views.socials import socials_routes
 from views.users import users_routes
+from views.cc import cc_routes
 
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24)
 app.register_blueprint(illordle_routes)
 app.register_blueprint(socials_routes)
 app.register_blueprint(users_routes)
+app.register_blueprint(cc_routes)
 
 # csp = {
 #     'default-src': '*'
@@ -63,43 +74,43 @@ def load_user(user_id):
 
 @login_manager.unauthorized_handler
 def unauthorized_callback():
-    return redirect("/login?state=" + urllib.parse.quote(request.path))
+    return redirect('/login?state=' + urllib.parse.quote(request.path))
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/login")
+@app.route('/login')
 def login():
-    state = request.args.get("state")
+    state = request.args.get('state')
 
     # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    authorization_endpoint = google_provider_cfg['authorization_endpoint']
 
     # Use library to construct the request for Google login and provide
     # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
-        state=state,
+        redirect_uri=request.base_url + '/callback',
+        scope=['openid', 'email', 'profile'],
+        state=state
     )
     return redirect(request_uri)
 
 
-@app.route("/login/callback")
+@app.route('/login/callback')
 def callback():
     # Get authorization code Google sent back to you
-    code = request.args.get("code")
-    state = request.args.get("state")
+    code = request.args.get('code')
+    state = request.args.get('state')
 
     # Find out what URL to hit to get tokens that allow you to ask for
     # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_endpoint = google_provider_cfg['token_endpoint']
 
     # Prepare and send a request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
@@ -121,36 +132,28 @@ def callback():
     # Now that you have tokens (yay) let's find and hit the URL
     # from Google that gives you the user's profile information,
     # including their Google profile image and email
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body).json()
 
     # You want to make sure their email is verified.
     # The user authenticated with Google, authorized your
     # app, and now you've verified their email through Google!
-    if userinfo_response.get("email_verified"):
-        unique_id = userinfo_response["sub"]
-        user_email = userinfo_response["email"]
-        user_name = userinfo_response["name"]
-        user_domain = userinfo_response.get("hd", "")
+    if userinfo_response.get('email_verified'):
+        unique_id = userinfo_response['sub']
+        user_email = userinfo_response['email']
+        user_name = userinfo_response['name']
         user_groups = get_groups_for_user(user_email)
 
         # Create or update user in db
         user = get_user(user_email)
         if user is None:
-            if user_domain == "illinimedia.com":
-                user = add_user(
-                    sub=unique_id, name=user_name, email=user_email, groups=user_groups
-                )
+            if user_email.endswith('@illinimedia.com'):
+                user = add_user(sub=unique_id, name=user_name, email=user_email, groups=user_groups)
             else:
-                return (
-                    "User must be a member of Illini Media for automatic registration.",
-                    403,
-                )
+                return 'User email must end with @illinimedia.com for automatic registration.', 403
         elif user.sub is None:
-            user = add_user(
-                sub=unique_id, name=user_name, email=user_email, groups=user_groups
-            )
+            user = add_user(sub=unique_id, name=user_name, email=user_email, groups=user_groups)
         elif user_groups != user.groups:
             update_user_groups(user, user_groups)
 
@@ -163,39 +166,73 @@ def callback():
             if not (parsed_url.scheme or parsed_url.netloc):
                 return redirect(url)
             else:
-                return "Illegal redirect URL.", 400
+                return 'Illegal redirect URL.', 400
 
-        return redirect(url_for("index"))
+        return redirect(url_for('index'))
     else:
-        return "User email not available or not verified by Google.", 400
+        return 'User email not available or not verified by Google.', 400
 
 
-@app.route("/api-query")
+@app.route('/api-query')
 @login_required
 def api_query():
-    return render_template("api_query.html")
+    return render_template('api_query.html')
+
+#TODO implement constant contact backend
+
+#remove later
+client_id = "d0d8d364-b7b6-497a-a8e7-6e89ac0c754d"
+client_secret = "A5sErJbUHdPu6pAHxo7x0w"
+redirect_uri = "https://cavemanfury.github.io/"
+
+"""
+@app.route('/constant-contact/login', methods=["GET"])
+@login_required
+def cc_login():
+    authorization_url = "https://authz.constantcontact.com/oauth2/default/v1/authorize"
+    redirect_uri = redirect_uri 
+    state = "state"  
+
+    # Params for authorization url
+    params = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "redirect_uri": redirect_uri,
+        "scope": "contact-data",
+        "response_type": "code",
+        "state": state
+    }
+    authorization_url += "?" + urllib.urlencode(params)
+    return redirect(authorization_url)
+
+@app.route('/constant-contact/login/callback', methods=["GET"])
+@login_required
+def cc_callback():
+    auth_code = request.args.get("code")
+    kv_store_set(auth_code)
+    redirect(url_for('index'))
+"""
 
 
-@app.route("/logout")
+
+@app.route('/logout')
 @login_required
 def logout():
-    if current_user.email.endswith("@illinimedia.com"):
+    if current_user.email.endswith('@illinimedia.com'):
         logout_user()
-        return redirect(url_for("yurr"))
+        return redirect(url_for('yurr'))
     else:
         logout_user()
-        return redirect(url_for("index"))
+        return redirect(url_for('index'))
 
 
-@app.route("/logout-success")
+@app.route('/logout-success')
 def yurr():
-    return render_template("yurr.html")
+    return render_template('yurr.html')
 
 
-if __name__ == "__main__":
-    if os.environ.get("DATASTORE_EMULATOR_HOST") is None:
-        logging.fatal("DATASTORE_EMULATOR_HOST environment variable must be set!")
+if __name__ == '__main__':
+    if os.environ.get('DATASTORE_EMULATOR_HOST') is None:
+        logging.fatal('DATASTORE_EMULATOR_HOST environment variable must be set!')
         exit(1)
-    app.jinja_env.auto_reload = True
-    app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.run(port=5001, ssl_context="adhoc")
+    app.run(port=5001, ssl_context='adhoc')
