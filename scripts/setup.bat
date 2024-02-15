f0fcc618c08b26ae2bb90a2d35dab3a7564962b6
rohitc33 <70339497+rohitc33@users.noreply.github.com>
2024-02-14 20:02:05 -0600

    Refactor Illordle word endpoints

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -2,3 +2,4 @@ CALL python3 -m venv .venv
 CALL .venv\Scripts\activate.bat
 CALL pip install --upgrade -r requirements.txt
 CALL pip install --upgrade -r requirements-dev.txt
+CALL gcloud auth application-default login
