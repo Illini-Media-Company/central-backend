86bc6345b313c5e58be5b3a6cbdd5a2d09ceb665
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2024-02-16 15:23:48 -0600

    Created function to post to twitter

--- util/stories.py
+++ util/stories.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import praw
 import requests
+import tweepy
 
 from constants import (
     REDDIT_USERNAME,
@@ -9,6 +10,10 @@ from constants import (
     REDDIT_CLIENT_SECRET,
     SUBREDDIT,
     FLAIR_ID,
+    TWITTER_API_KEY,
+    TWITTER_API_SECRET_KEY,
+    TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET
 )
 
 
@@ -32,3 +37,27 @@ def post_to_reddit(title, url):
     subreddit = reddit.subreddit(SUBREDDIT)
     submission = subreddit.submit(title, url=url, flair_id=FLAIR_ID)
     return "https://www.reddit.com" + submission.permalink
+
+def post_to_twitter(title, url):
+    API_KEY = TWITTER_API_KEY
+    API_SECRET_KEY = TWITTER_API_SECRET_KEY
+    ACCESS_TOKEN = TWITTER_ACCESS_TOKEN
+    ACCESS_TOKEN_SECRET = TWITTER_ACCESS_TOKEN_SECRET
+
+    # Handling Twitter authentication
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # Creating API object
+    api = tweepy.API(auth)
+
+    # Combining title and URL to form tweet content
+    tweet = f"{title} {url}"
+
+    # Tweeting
+    status = api.update_status(status=tweet)
+
+    # Return the tweet URL (assuming tweet was successful)
+    tweet_id = status.id
+    tweet_url = f"https://twitter.com/user/status/{tweet_id}"
+    return tweet_url
