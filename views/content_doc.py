5c169d79c02980865b6cf17c2469c4a8059f3601
rohitc33 <70339497+rohitc33@users.noreply.github.com>
2024-03-06 17:17:26 -0600

    Secure copy editing endpoint

--- views/content_doc.py
+++ views/content_doc.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request
+from flask_cors import cross_origin
 
+from constants import APPS_SCRIPT_RUNNER_EMAIL, CONTEND_DOC_AUD
 from util.copy_editing import notify_current_copy_editor
+from util.security import csrf, restrict_to
 
 
 content_doc_routes = Blueprint(
@@ -9,8 +12,14 @@ content_doc_routes = Blueprint(
 
 
 @content_doc_routes.route("/send-to-copy", methods=["POST"])
+@csrf.exempt
+@cross_origin()
+@restrict_to(
+    [APPS_SCRIPT_RUNNER_EMAIL, "editors", "webdev"], google_id_token_aud=CONTEND_DOC_AUD
+)
 def send_story_to_copy():
     story_url = request.form["story_url"]
     copy_chief_email = request.form["copy_chief_email"]
 
-    return notify_current_copy_editor(story_url, copy_chief_email)
+    notify_current_copy_editor(story_url, copy_chief_email)
+    return "Slack message sent.", 200
