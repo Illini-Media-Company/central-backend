c478205e63c83890a8f79f72dae420f08104f20c
rohitc33 <70339497+rohitc33@users.noreply.github.com>
2024-04-03 19:14:35 -0500

    Update pre-commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,5 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade -r requirements.txt
 pip install --upgrade -r requirements-dev.txt
+pre-commit install
 gcloud auth application-default login
