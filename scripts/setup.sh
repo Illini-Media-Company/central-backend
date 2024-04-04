6ac81f2ef45158845331533e14f49b56cead4ea1
rohitc33 <70339497+rohitc33@users.noreply.github.com>
2024-04-03 19:17:17 -0500

    Unset hooksPath in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,5 +4,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade -r requirements.txt
 pip install --upgrade -r requirements-dev.txt
+git config --unset core.hooksPath
 pre-commit install
 gcloud auth application-default login
