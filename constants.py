4e5c4def656c227f397342ae5041b5237ea2304a
Talha Ali <68120794+Cavemanfury@users.noreply.github.com>
2024-04-22 16:01:50 -0500

    Update constants.py
    Added daily-urbana and daily-champaign list ids

--- constants.py
+++ constants.py
@@ -37,6 +37,8 @@ CC_LIST_MAPPING = {
     "headline_news": "01d15d60-4c11-11e3-826c-d4ae52725666",
     "sports": "0fa0f9d0-f33e-11e8-bed9-d4ae52733d3a",
     "wpgu": "01ff9950-4c11-11e3-826c-d4ae52725666",
+    "daily-urbana": "8f76cac0-dfdf-11ee-9c51-fa163e5bc304",
+    "daily-champaign": "4ab95568-f215-11ee-9e0c-fa163e7b09ec"
 }
 
 RETOOL_API_KEY = os.environ.get("RETOOL_API_KEY", None)
