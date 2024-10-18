c3fb1198901281c57069afc368cbb6e244b84d82
Yaxin203 <91151081+Yaxin203@users.noreply.github.com>
2024-10-18 16:30:53 -0500

    finished up quicklinks

--- views/quick_links.py
+++ views/quick_links.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 from db.quick_link import get_all_quick_links, add_quick_link, remove_quick_link
+from db.group import get_all_groups
 from util.security import restrict_to
 
 
@@ -16,7 +17,8 @@ quick_links_routes = Blueprint(
 def dashboard():
     # TODO: Create dashboard for editing quick links
     links = get_all_quick_links()
-    return render_template("quicklink.html", links=links)
+    groups = get_all_groups()
+    return render_template("quicklink.html", links=links, groups=groups)
 
 
 @quick_links_routes.route("", methods=["GET"])
@@ -41,7 +43,7 @@ def create_quick_link():
 @login_required
 @restrict_to(["student-managers", "editors", "imc-staff-webdev"])
 def delete_quick_link(uid):
-    if uid.isdigit() and remove_quick_link(int(uid)):
+    if remove_quick_link(int(uid)):
         return "Quick link deleted.", 200
     else:
         return "Quick link not found.", 404
