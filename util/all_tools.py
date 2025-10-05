65f524748151db2373c89799ff4f79d23594f5aa
slabosz <112522465+slabosz@users.noreply.github.com>
2025-10-05 15:32:51 -0500

    Adding comments

--- util/all_tools.py
+++ util/all_tools.py
@@ -1,3 +1,9 @@
+# This file contains helper functions for the Tools API
+#
+# Created by Jacob Slabosz on Oct. 1, 2025
+# Last modified Oct. 1, 2025
+
+
 def format_restricted_groups(groups):
     """Accepts a list of restricted groups and reformats it as a string for correct display on user-facing pages."""
 
