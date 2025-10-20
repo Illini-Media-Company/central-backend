d853bbd4498d1b70837742bbd55fdf3cc7e44fa3
adithya_raman <adithyaraman76@gmail.com>
2025-10-20 11:46:42 -0500

    feat: brand new frontend portal for onboarding
    Integrated a brand new onboarding dashboard for both users and admins with IMC central backend. Next step is to combine with the python backend.

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ from views.copy_schedule import copy_schedule_routes
 from views.map_points import map_points_routes
 from views.overlooked import overlooked_routes
 from views.food_truck import food_truck_routes
+from views.contract_automization import onboarding_routes
 
 from util.helpers.ap_datetime import (
     ap_datetime,
@@ -110,6 +111,7 @@ app.register_blueprint(copy_schedule_routes)
 app.register_blueprint(map_points_routes)
 app.register_blueprint(overlooked_routes)
 app.register_blueprint(food_truck_routes)
+app.register_blueprint(onboarding_routes)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
