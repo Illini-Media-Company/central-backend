7c8e1402fb339cd05de198bde49f361f85386bc1
slabosz <112522465+slabosz@users.noreply.github.com>
2025-11-16 21:58:04 -0600

    Changes to employee agreement

--- views/employee_agreement.py
+++ views/employee_agreement.py
@@ -1,46 +1,58 @@
 from flask import Blueprint, request, render_template
-from util.slackbots.slackbot import app
 from util.security import restrict_to
 from flask_login import current_user, login_required
 from util.slackbots.employee_agreement_slackbot import (
     send_employee_agreement_notification,
+    send_reviewer_notification,
+    send_confirmation_notification,
 )
 from db.employee_agreement import (
     add_employee_agreement,
-    get_employee_agreements_by_user,
+    sign_update_agreement,
+    get_agreement_name,
+    get_pending_agreements_for_user,
     get_pending_agreements_for_editor,
     get_pending_agreements_for_manager,
     get_pending_agreements_for_chief,
-    get_agreement_objects_by_user,
+    get_past_agreements_for_user,
+    get_incomplete_agreements,
+    remove_agreement,
+    get_agreement_by_id,
+    update_slack_info,
 )
-from zoneinfo import ZoneInfo
-from datetime import datetime
 
 employee_agreement_routes = Blueprint(
     "employee_agreement_routes", __name__, url_prefix="/employee-agreement"
 )
-from google.cloud import ndb
-
-client = ndb.Client()
-
-# we need to get the user_slack_id and agreement_url from the request this will be from the employee agreement db
-# do we need to get a get, which is the input from the user and then post to send the notification using the input from the manager
-# after that we need to get the update from the signature as another get and that tells us when to send the follow up notification
-"""
-@employee_agreement_routes.route("/send-notification", methods=["POST"])
-@login_required
-def send_notification():
-    send_employee_agreement_notification("U09LTPY3MSP", "http://example.com/agreement")
-    return "Notification sent", 200
-"""
 
 
 @employee_agreement_routes.route("/dashboard", methods=["GET"])
 @login_required
 def dashboard():
+    # Get the user's name (used for the signature)
     user_name = current_user.name
+
+    pending_agreements = []
+    logged_in_user_email = current_user.email
+
+    # Get pending agreements treating user as employee, editor, manager and EIC
+    user_agreements = get_pending_agreements_for_user(logged_in_user_email)
+    editor_agreements = get_pending_agreements_for_editor(logged_in_user_email)
+    manager_agreements = get_pending_agreements_for_manager(logged_in_user_email)
+    chief_agreements = get_pending_agreements_for_chief(logged_in_user_email)
+
+    pending_agreements.extend(user_agreements)
+    pending_agreements.extend(editor_agreements)
+    pending_agreements.extend(manager_agreements)
+    pending_agreements.extend(chief_agreements)
+
+    past_agreements = get_past_agreements_for_user(logged_in_user_email)
+
     return render_template(
-        "di_contract_automization_dashboard.html", currentUserName=user_name
+        "employee_agreement_dashboard.html",
+        currentUserName=user_name,
+        agreements=pending_agreements,
+        past_agreements=past_agreements,
     )
 
 
@@ -48,164 +60,122 @@ def dashboard():
 @login_required
 @restrict_to(["imc-staff-webdev", "di-section-editors"])
 def admin_dashboard():
-    return render_template("di_contract_automization_admin.html")
+    # Handle pagination of in-progress agreements
+    page_inprog = int(request.args.get("page-inprog", 1))
+    PER_PAGE_INPROG = 8
+
+    all_inprog = get_incomplete_agreements()
+    total_inprog = len(all_inprog)
+
+    start_inprog = (page_inprog - 1) * PER_PAGE_INPROG
+    end_inprog = start_inprog + PER_PAGE_INPROG
+    inprogress = all_inprog[start_inprog:end_inprog]
+
+    return render_template(
+        "employee_agreement_admin.html",
+        inprogress_agreements=inprogress,
+        total_inprog=total_inprog,
+        page_inprog=page_inprog,
+        per_page_inprog=PER_PAGE_INPROG,
+    )
 
 
-@employee_agreement_routes.route("/send-notification", methods=["POST"])
+@employee_agreement_routes.route("/submit", methods=["POST"])
 @login_required
 def send_notification():
     data = request.get_json()
     emails = data.get("emails", [])
 
     if not emails:
-        return "No emails provided", 400
+        return "No emails provided.", 400
+
+    # Extract editor, manager, EIC emails
     manager_email = data.get("manager", "")
     chief_email = data.get("chief", "")
     editor_email = data.get("editor", "")
     url = data.get("agreement_link", "")
+    agr_name = data.get("agreement_name", "")
 
+    if (
+        not manager_email
+        or not chief_email
+        or not editor_email
+        or not url
+        or not agr_name
+    ):
+        return "Missing required information.", 400
+
+    # Add an agreement for each user email
     for email in emails:
         email = email.strip()
 
-        user_data = app.client.users_lookupByEmail(email=email)
+        # Add to DB
+        temp = add_employee_agreement(
+            user_email=email,
+            editor_email=editor_email,
+            manager_email=manager_email,
+            chief_email=chief_email,
+            agreement_url=url,
+            agreement_name=agr_name,
+        )
 
-        # we need to do the lookup by email for hring manager, editor, cheif
-        # assume that the hiring manager would be the only one on this page
-        editor_data = app.client.users_lookupByEmail(email=editor_email)
-        if not editor_data.get("ok"):
-            return "editor manager not found", 404
-        editor_slack_id = editor_data["user"]["id"]
+        uid = temp.get("uid")
 
-        manager_data = app.client.users_lookupByEmail(email=manager_email)
-        if not manager_data.get("ok"):
-            return "Manager not found", 404
-        manager_slack_id = manager_data["user"]["id"]
+        # Send notification to the user
+        res = send_employee_agreement_notification(email, agr_name)
+        if not res.get("ok"):
+            return "Failed to notify a user.", 400
 
-        chief_data = app.client.users_lookupByEmail(email=chief_email)
-        if not chief_data.get("ok"):
-            return "Chief not found", 404
-        chief_slack_id = chief_data["user"]["id"]
+        ch = res.get("channel")
+        ts = res.get("ts")
 
-        if user_data.get("ok"):
-            user_slack_id = user_data["user"]["id"]
-            agreement_url = url
+        print(ch, ts)
 
-            add_employee_agreement(
-                user_id=user_slack_id,
-                editor_id=editor_slack_id,
-                manager_id=manager_slack_id,
-                chief_id=chief_slack_id,
-                agreement_url=agreement_url,
-            )
-
-            # we need to add the user to the db and then call the notification function
-            send_employee_agreement_notification(user_slack_id)
-        else:
-            return f"User with email {email} not found", 404
-    return "Emails sucessfully sent", 200
-
-
-@employee_agreement_routes.route("/get-pending-signatures", methods=["GET"])
-@login_required
-def get_pending_signatures():
-    logged_in_user_email = current_user.email
-    user_data = app.client.users_lookupByEmail(email=logged_in_user_email)
-    if not user_data.get("ok"):
-        return "User not found", 404
-    user_slack_id = user_data["user"]["id"]
-    pending_agreements = []
-    my_agreements_list = get_employee_agreements_by_user(user_slack_id)
-
-    for agreement in my_agreements_list:
-        if agreement["user_signed"] is None:
-            pending_agreements.append(agreement)
-    # Check for pending agreements as editor
-    editor_agreements = get_pending_agreements_for_editor(user_slack_id)
-    pending_agreements.extend(editor_agreements)
-    # Check for pending agreements as manager
-    manager_agreements = get_pending_agreements_for_manager(user_slack_id)
-    pending_agreements.extend(manager_agreements)
-    # Check for pending agreements as chief
-    chief_agreements = get_pending_agreements_for_chief(user_slack_id)
-    pending_agreements.extend(chief_agreements)
-    return {"pending_agreements": pending_agreements}, 200
+        res = update_slack_info(int(uid), ch, ts)
+        if not res:
+            return "Error updating EmployeeAgreement object.", 400
+    return "Notifications successfully sent.", 200
 
 
 # called after the user signs the agreement on the webpage
-@employee_agreement_routes.route("/sign-agreement", methods=["POST"])
+@employee_agreement_routes.route("/<uid>/sign", methods=["POST"])
 @login_required
-def sign_agreement():
-    data = request.get_json()
-    agreement_user_id = data.get("agreement_user_id")
-    if not agreement_user_id:
-        return "Agreement ID is required", 400
+def sign_agreement(uid):
     logged_in_user_email = current_user.email
-    user_data = app.client.users_lookupByEmail(email=logged_in_user_email)
-    if not user_data.get("ok"):
-        return "User not found", 404
-    signer_slack_id = user_data["user"]["id"]
-    agreement_list = get_agreement_objects_by_user(agreement_user_id)
-    if not agreement_list:
-        return "Agreement not found", 404
 
-    agreement = None
+    # Sign the agreement in the DB
+    status, employee, next = sign_update_agreement(int(uid), logged_in_user_email)
 
-    for agr in agreement_list:
-        if signer_slack_id == agr.user_id and agr.user_signed is None:
-            agreement = agr
-            break
-        elif signer_slack_id == agr.editor_id and agr.editor_signed is None:
-            agreement = agr
-            break
-        elif signer_slack_id == agr.manager_id and agr.manager_signed is None:
-            agreement = agr
-            break
-        elif signer_slack_id == agr.chief_id and agr.chief_signed is None:
-            agreement = agr
-            break
+    if status == False:
+        return "Error signing agreement", 400
 
-    if not agreement:
-        return (
-            "You are not authorized to sign this agreement, it's not your turn, or it has already been signed",
-            403,
-        )
+    agr_name = get_agreement_name(int(uid))
+    if agr_name is None:
+        agr_name = "Agreement"
 
-    next_signer_id = None
-    next_signer_role = None
-
-    if signer_slack_id == agreement.user_id and agreement.user_signed is None:
-        agreement.user_signed = datetime.now(tz=ZoneInfo("America/Chicago"))
-        next_signer_id = agreement.editor_id
-        next_signer_role = "editor"
-    elif signer_slack_id == agreement.editor_id and agreement.editor_signed is None:
-        agreement.editor_signed = datetime.now(tz=ZoneInfo("America/Chicago"))
-        next_signer_id = agreement.manager_id
-        next_signer_role = "manager"
-    elif signer_slack_id == agreement.manager_id and agreement.manager_signed is None:
-        agreement.manager_signed = datetime.now(tz=ZoneInfo("America/Chicago"))
-        next_signer_id = agreement.chief_id  # FIX: Set next signer
-        next_signer_role = "chief"
-    elif signer_slack_id == agreement.chief_id and agreement.chief_signed is None:
-        agreement.chief_signed = datetime.now(tz=ZoneInfo("America/Chicago"))
+    # Notify the next person (if there is a next person)
+    if next:
+        res = send_reviewer_notification(next, employee, agr_name)
+        if res.get("ok") == False:
+            return "Error notifying next signer.", 400
     else:
-        return (
-            "You are not authorized to sign this agreement or have already signed",
-            403,
-        )
+        temp = get_agreement_by_id(int(uid))
+        ch = temp.get("slack_ch")
+        ts = temp.get("slack_ts")
 
-    with client.context():
-        agreement.put()
+        res = send_confirmation_notification(employee, agr_name, ch, ts)
+        if res.get("ok") == False:
+            return "Error sending confirmation message.", 400
 
-    if next_signer_id:  # Check if there *is* a next signer
-        pending_list = []
-        if next_signer_role == "editor":
-            pending_list = get_pending_agreements_for_editor(next_signer_id)
-        elif next_signer_role == "manager":
-            pending_list = get_pending_agreements_for_manager(next_signer_id)
-        elif next_signer_role == "chief":
-            pending_list = get_pending_agreements_for_chief(next_signer_id)
+    return "Agreement signed successfully.", 200
 
-        if len(pending_list) == 1:
-            send_employee_agreement_notification(next_signer_id)
 
-    return "Agreement signed successfully", 200
+@employee_agreement_routes.route("/<uid>/remove", methods=["POST"])
+@login_required
+def delete_agreement(uid):
+    res = remove_agreement(int(uid))
+
+    if res:
+        return "Successfully deleted agreement.", 200
+    else:
+        return "Failed to delete agreement.", 400
